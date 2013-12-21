a1d828f3e4527fd3d5f5923f870b1ee46b3f9ec1
Ricardo Cruz <ricardocruz.a89@gmail.com>
2013-12-21 16:00:34 -0600

    added the bridges routes and template

--- js/app.js
+++ js/app.js
@@ -4,11 +4,57 @@ var LaredoCamApp = Ember.Application.create({
 });
 
 // Data
-App.BRIDGES = [
+LaredoCamApp.BRIDGES = [
+  {
+    id: '1',
+    title: 'Puente de las Americas Bridge I',
+    images: {
+      us: 'http://www.ci.laredo.tx.us/bridgesys/hugeb1cam1.jpg',
+      mx: 'http://www.ci.laredo.tx.us/bridgesys/hugeb1cam2.jpg'
+    }
+  },
+  {
+    id: '2',
+    title: 'Juarez Lincoln Bridge',
+    images: {
+      us: 'http://www.ci.laredo.tx.us/bridgesys/hugeb2cam1.jpg',
+      mx: 'http://www.ci.laredo.tx.us/bridgesys/hugeb2cam2.jpg'
+    }
+  },
+  {
+    id: '3',
+    title: 'Colombia Bridge',
+    images: {
+      us: 'http://www.ci.laredo.tx.us/bridgesys/hugeb3cam2.jpg',
+      mx: 'http://www.ci.laredo.tx.us/bridgesys/hugeb3cam1.jpg'
+    }
+  },
+  {
+    id: '4',
+    title: 'World Trade Bridge',
+    images: {
+      us: 'http://www.ci.laredo.tx.us/bridgesys/hugeb4cam2.jpg',
+      mx: 'http://www.ci.laredo.tx.us/bridgesys/hugeb4cam1.jpg'
+    }
+  }
 ];
 
 // Router
+LaredoCamApp.Router.map(function() {
+  this.resource('cam', {path: '/cams/:id'});
+})
 
 // Routes
+LaredoCamApp.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return LaredoCamApp.BRIDGES;
+  }
+});
+
+LaredoCamApp.CamRoute = Ember.Route.extend({
+  model: function(params) {
+    return LaredoCamApp.BRIDGES.findBy('id', params.id);
+  }
+});
 
 // Controllers
