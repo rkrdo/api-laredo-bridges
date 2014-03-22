c55552d7f2fa4a8a5690aeb6ac6fba8cba9ee2b5
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-03-22 17:57:33 -0500

    Added the route to make the request in the sinatra app

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'sinatra'
+require 'lib/bwt/general'
+require 'lib/bwt/laredo'
+require 'json'
 
 get "/" do
   send_file "public/index.html"
+end
+
+get "/bwt/:bridge_num.json" do
+  content_type :json
+  BWT::Laredo.send("bridge_#{params[:bridge_num]}").to_json
 end
