f5bc4648564c58322635943e3e5301d550fb4f08
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-03-22 18:50:31 -0500

    Manage the request via a number ID

--- app.rb
+++ app.rb
@@ -5,11 +5,18 @@ require 'lib/bwt/general'
 require 'lib/bwt/laredo'
 require 'json'
 
+BRIDGES =  {
+  "1" => "one",
+  "2" => "two",
+  "3" => "three",
+  "4" => "four"
+}
+
 get "/" do
   send_file "public/index.html"
 end
 
-get "/bwt/:bridge_num.json" do
+get "/laredo/:bridge_num.json" do
   content_type :json
-  BWT::Laredo.send("bridge_#{params[:bridge_num]}").to_json
+  BWT::Laredo.send("bridge_#{BRIDGES[params[:bridge_num].to_s]}").to_json
 end
