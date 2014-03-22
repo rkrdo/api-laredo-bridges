d466d79ad7e9435428510a06897907bde2d69976
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-03-22 17:37:08 -0500

    Refactored Laredo class

--- lib/bwt/laredo.rb
+++ lib/bwt/laredo.rb
@@ -2,31 +2,21 @@ module BWT
   class Laredo < BWT::General
     class << self
 
+      BRIDGES = {
+        one: "230401",
+        two: "230402",
+        three: "230403",
+        four: "230404"
+      }
+
       def all_ports
         super.select { |p| p[:port_name] == "Laredo" }
       end
 
-      def bridge_one
-        find_bridge 230401
+      def method_missing(name, *args)
+        super unless name =~ /^bridge_(.*)$/ && BRIDGES.keys.include?($1.to_sym)
+        all_ports.select { |p| p[:port_number] == BRIDGES[$1.to_sym] }.first
       end
-
-      def bridge_two
-        find_bridge 230402
-      end
-
-      def bridge_three
-        find_bridge 230403
-      end
-
-      def bridge_four
-        find_bridge 230404
-      end
-
-      private
-
-        def find_bridge(port_number)
-          all_ports.select{ |p| p[:port_number] == port_number.to_s }.first
-        end
 
     end
   end
--- spec/lib/bwt/laredo_spec.rb
+++ spec/lib/bwt/laredo_spec.rb
@@ -9,34 +9,51 @@ module BWT
       stub_request(:get, 'http://apps.cbp.gov/bwt/bwt.xml').to_return(body: xml_response)
     end
 
-    describe "#all_ports" do
-      it "returns all ports from Laredo" do
-        laredo.all_ports.must_equal LAREDO_HASH
+    describe "valid bridge methods" do
+      describe "#all_ports" do
+        it "returns all ports from Laredo" do
+          laredo.all_ports.must_equal LAREDO_HASH
+        end
+      end
+
+      describe "#bridge_one" do
+        it "returns info in bridge one" do
+          laredo.bridge_one.must_equal LAREDO_HASH[0]
+        end
+      end
+
+      describe "#bridge_two" do
+        it "returns info in bridge two" do
+          laredo.bridge_two.must_equal LAREDO_HASH[1]
+        end
+      end
+
+      describe "#bridge_three" do
+        it "returns info in bridge three" do
+          laredo.bridge_three.must_equal LAREDO_HASH[2]
+        end
+      end
+
+      describe "#bridge_four" do
+        it "returns info in bridge four" do
+          laredo.bridge_four.must_equal LAREDO_HASH[3]
+        end
       end
     end
 
-    describe "#bridge_one" do
-      it "returns info in bridge one" do
-        laredo.bridge_one.must_equal LAREDO_HASH[0]
+    describe "non-valid bridge methods" do
+      describe "bridge number methods" do
+        it "raises an exception" do
+          -> { laredo.bridge_five }.must_raise NoMethodError
+        end
       end
-    end
 
-    describe "#bridge_two" do
-      it "returns info in bridge two" do
-        laredo.bridge_two.must_equal LAREDO_HASH[1]
+      describe "methods that end with a valid _number" do
+        it "raises an exception" do
+          -> { laredo.test_one }.must_raise NoMethodError
+        end
       end
-    end
 
-    describe "#bridge_three" do
-      it "returns info in bridge three" do
-        laredo.bridge_three.must_equal LAREDO_HASH[2]
-      end
-    end
-
-    describe "#bridge_four" do
-      it "returns info in bridge four" do
-        laredo.bridge_four.must_equal LAREDO_HASH[3]
-      end
     end
   end
 end
