1e9137a3df0b5e8e208e9428e7232e18ecdf8d96
Ricardo Cruz <ricardocruz.a89@gmail.com>
2014-03-22 21:35:55 -0500

    Gave format to the Laredo Hash and added a cam number

--- spec/utils/laredo_hash.rb
+++ spec/utils/laredo_hash.rb
@@ -1,4 +1,244 @@
-LAREDO_HASH = [{"port_number"=>"230401", "border"=>"Mexican Border", "port_name"=>"Laredo", "crossing_name"=>"Bridge I", "hours"=>"24 hrs/day", "date"=>"3/15/2014", "port_status"=>"Open", "commercial_vehicle_lanes"=>{"maximum_lanes"=>"N/A", "standard_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}, "FAST_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}}, "passenger_vehicle_lanes"=>{"maximum_lanes"=>"4", "standard_lanes"=>{"update_time"=>"At 2:00 pm CDT", "operational_status"=>"delay", "delay_minutes"=>"1 hrs 35 min", "lanes_open"=>"4"}, "NEXUS_SENTRI_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}, "ready_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}}, "pedestrian_lanes"=>{"maximum_lanes"=>"9", "standard_lanes"=>{"update_time"=>"At 2:00 pm CDT", "operational_status"=>"delay", "delay_minutes"=>"1 hrs 15 min", "lanes_open"=>"4"}, "ready_lanes"=>{"update_time"=>"At 2:00 pm CDT", "operational_status"=>"delay", "delay_minutes"=>"1 hrs 15 min", "lanes_open"=>"2"}}},
-{"port_number"=>"230402", "border"=>"Mexican Border", "port_name"=>"Laredo", "crossing_name"=>"Bridge II", "hours"=>"24 hrs/day", "date"=>"3/15/2014", "port_status"=>"Open", "commercial_vehicle_lanes"=>{"maximum_lanes"=>"N/A", "standard_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}, "FAST_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}}, "passenger_vehicle_lanes"=>{"maximum_lanes"=>"15", "standard_lanes"=>{"update_time"=>"At 2:00 pm CDT", "operational_status"=>"delay", "delay_minutes"=>"1 hrs 30 min", "lanes_open"=>"10"}, "NEXUS_SENTRI_lanes"=>{"update_time"=>"At 2:00 pm CDT", "operational_status"=>"delay", "delay_minutes"=>"15 min", "lanes_open"=>"2"}, "ready_lanes"=>{"update_time"=>"At 2:00 pm CDT", "operational_status"=>"delay", "delay_minutes"=>"1 hrs 30 min", "lanes_open"=>"3"}}, "pedestrian_lanes"=>{"maximum_lanes"=>"N/A", "standard_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}, "ready_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}}, "construction_notice"=>"Ready Lanes are open from 8AM - Midnight seven days a week.\n\nGo to www.GetYouHome.gov for more information."},
-{"port_number"=>"230403", "border"=>"Mexican Border", "port_name"=>"Laredo", "crossing_name"=>"Colombia Solidarity", "hours"=>"8 am-Midnight", "date"=>"3/15/2014", "port_status"=>"Closed", "commercial_vehicle_lanes"=>{"maximum_lanes"=>"8", "standard_lanes"=>{"update_time"=>"At 2:00 pm CDT", "operational_status"=>"no delay", "delay_minutes"=>"5", "lanes_open"=>"2"}, "FAST_lanes"=>{"update_time"=>"At 2:00 pm CDT", "operational_status"=>"no delay", "delay_minutes"=>"0", "lanes_open"=>"1"}}, "passenger_vehicle_lanes"=>{"maximum_lanes"=>"4", "standard_lanes"=>{"update_time"=>"At 2:00 pm CDT", "operational_status"=>"delay", "delay_minutes"=>"30 min", "lanes_open"=>"3"}, "NEXUS_SENTRI_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}, "ready_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}}, "pedestrian_lanes"=>{"maximum_lanes"=>"N/A", "standard_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}, "ready_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}}},
-{"port_number"=>"230404", "border"=>"Mexican Border", "port_name"=>"Laredo", "crossing_name"=>"World Trade Bridge", "hours"=>"8 am-Midnight", "date"=>"3/15/2014", "port_status"=>"Closed", "commercial_vehicle_lanes"=>{"maximum_lanes"=>"16", "standard_lanes"=>{"update_time"=>"At 2:00 pm CDT", "operational_status"=>"delay", "delay_minutes"=>"30 min", "lanes_open"=>"8"}, "FAST_lanes"=>{"update_time"=>"At 2:00 pm CDT", "operational_status"=>"delay", "delay_minutes"=>"10 min", "lanes_open"=>"2"}}, "passenger_vehicle_lanes"=>{"maximum_lanes"=>"N/A", "standard_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}, "NEXUS_SENTRI_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}, "ready_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}}, "pedestrian_lanes"=>{"maximum_lanes"=>"N/A", "standard_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}, "ready_lanes"=>{"operational_status"=>"N/A", "update_time"=>nil, "delay_minutes"=>nil, "lanes_open"=>nil}}}]
+LAREDO_HASH = [
+  {
+    "port_number"=>"230401",
+    "border"=>"Mexican Border",
+    "port_name"=>"Laredo",
+    "crossing_name"=>"Bridge I",
+    "hours"=>"24 hrs/day",
+    "date"=>"3/15/2014",
+    "port_status"=>"Open",
+    "commercial_vehicle_lanes"=>{
+      "maximum_lanes"=>"N/A",
+      "standard_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      },
+      "FAST_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      }
+    },
+    "passenger_vehicle_lanes"=>{
+      "maximum_lanes"=>"4",
+      "standard_lanes"=>{
+        "update_time"=>"At 2:00 pm CDT",
+        "operational_status"=>"delay",
+        "delay_minutes"=>"1 hrs 35 min",
+        "lanes_open"=>"4"
+      },
+      "NEXUS_SENTRI_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      },
+      "ready_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      }
+    },
+    "pedestrian_lanes"=>{
+      "maximum_lanes"=>"9",
+      "standard_lanes"=>{
+        "update_time"=>"At 2:00 pm CDT",
+        "operational_status"=>"delay",
+        "delay_minutes"=>"1 hrs 15 min",
+        "lanes_open"=>"4"
+      },
+      "ready_lanes"=>{
+        "update_time"=>"At 2:00 pm CDT",
+        "operational_status"=>"delay",
+        "delay_minutes"=>"1 hrs 15 min",
+        "lanes_open"=>"2",
+      }
+    },
+    "cam_id"=>"1"
+  },
+  {
+    "port_number"=>"230402",
+    "border"=>"Mexican Border",
+    "port_name"=>"Laredo",
+    "crossing_name"=>"Bridge II",
+    "hours"=>"24 hrs/day",
+    "date"=>"3/15/2014",
+    "port_status"=>"Open",
+    "commercial_vehicle_lanes"=>{
+      "maximum_lanes"=>"N/A",
+      "standard_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      },
+      "FAST_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      }
+    },
+    "passenger_vehicle_lanes"=>{
+      "maximum_lanes"=>"15",
+      "standard_lanes"=>{
+        "update_time"=>"At 2:00 pm CDT",
+        "operational_status"=>"delay",
+        "delay_minutes"=>"1 hrs 30 min",
+        "lanes_open"=>"10"
+      },
+      "NEXUS_SENTRI_lanes"=>{
+        "update_time"=>"At 2:00 pm CDT",
+        "operational_status"=>"delay",
+        "delay_minutes"=>"15 min",
+        "lanes_open"=>"2"
+      },
+      "ready_lanes"=>{
+        "update_time"=>"At 2:00 pm CDT",
+        "operational_status"=>"delay",
+        "delay_minutes"=>"1 hrs 30 min",
+        "lanes_open"=>"3"
+      }
+    },
+    "pedestrian_lanes"=>{
+      "maximum_lanes"=>"N/A",
+      "standard_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      },
+      "ready_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      }
+    },
+    "construction_notice"=>"Ready Lanes are open from 8AM - Midnight seven days a week.\n\nGo to www.GetYouHome.gov for more information.",
+    "cam_id"=>"2"
+  },
+  {
+    "port_number"=>"230403",
+    "border"=>"Mexican Border",
+    "port_name"=>"Laredo",
+    "crossing_name"=>"Colombia Solidarity",
+    "hours"=>"8 am-Midnight",
+    "date"=>"3/15/2014",
+    "port_status"=>"Closed",
+    "commercial_vehicle_lanes"=>{
+      "maximum_lanes"=>"8",
+      "standard_lanes"=>{
+        "update_time"=>"At 2:00 pm CDT",
+        "operational_status"=>"no delay",
+        "delay_minutes"=>"5",
+        "lanes_open"=>"2"
+      },
+      "FAST_lanes"=>{
+        "update_time"=>"At 2:00 pm CDT",
+        "operational_status"=>"no delay",
+        "delay_minutes"=>"0", "lanes_open"=>"1"
+      }
+    },
+    "passenger_vehicle_lanes"=>{
+      "maximum_lanes"=>"4",
+      "standard_lanes"=>{
+        "update_time"=>"At 2:00 pm CDT",
+        "operational_status"=>"delay",
+        "delay_minutes"=>"30 min",
+        "lanes_open"=>"3"},
+        "NEXUS_SENTRI_lanes"=>{
+          "operational_status"=>"N/A",
+          "update_time"=>nil,
+          "delay_minutes"=>nil,
+          "lanes_open"=>nil
+        },
+        "ready_lanes"=>{
+          "operational_status"=>"N/A",
+          "update_time"=>nil,
+          "delay_minutes"=>nil,
+          "lanes_open"=>nil
+        }
+    },
+    "pedestrian_lanes"=>{
+      "maximum_lanes"=>"N/A",
+      "standard_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      }, "ready_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      }
+    },
+    "cam_id"=> "3"
+  },
+  {
+    "port_number"=>"230404",
+    "border"=>"Mexican Border",
+    "port_name"=>"Laredo",
+    "crossing_name"=>"World Trade Bridge",
+    "hours"=>"8 am-Midnight",
+    "date"=>"3/15/2014",
+    "port_status"=>"Closed",
+    "commercial_vehicle_lanes"=>{
+      "maximum_lanes"=>"16",
+      "standard_lanes"=>{
+        "update_time"=>"At 2:00 pm CDT",
+        "operational_status"=>"delay",
+        "delay_minutes"=>"30 min",
+        "lanes_open"=>"8"
+      },
+      "FAST_lanes"=>{
+        "update_time"=>"At 2:00 pm CDT",
+        "operational_status"=>"delay",
+        "delay_minutes"=>"10 min",
+        "lanes_open"=>"2"
+      }
+    },
+    "passenger_vehicle_lanes"=>{
+      "maximum_lanes"=>"N/A",
+      "standard_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      },
+      "NEXUS_SENTRI_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      },
+      "ready_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      }
+    },
+    "pedestrian_lanes"=>{
+      "maximum_lanes"=>"N/A",
+      "standard_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      },
+      "ready_lanes"=>{
+        "operational_status"=>"N/A",
+        "update_time"=>nil,
+        "delay_minutes"=>nil,
+        "lanes_open"=>nil
+      }
+    },
+    "cam_id"=>"4"
+  }
+]
